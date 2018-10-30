48995ed812dafb74ec967c0f829bc14ec2803dfd
RajinBojan <bojan_rajin@yahoo.com>
2018-10-30 18:49:34 +0100

    dodat ispis Hello Mile

--- src/com/helloworld/HelloWorld.java
+++ src/com/helloworld/HelloWorld.java
@@ -6,6 +6,8 @@ public class HelloWorld {
 		System.out.println("Hello World");
 		System.out.println("Hello ");
 		System.out.println("Hello again ");
+		String a = "Mile";
+		System.out.println("Hello " + a);
 	}
 
 }
